bed0f68398df7996af437091b2f5d8005cc1223d
Mohande3Khademi <Mohande3Khademi@hotmail.com>
2024-01-04 13:03:13 +0330

    Add project files.

---
+++ TDD_UserManagement/UserManagementTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_UserManagement
+{
+
+    //todo : FEATURE : 
+    //! Add
+    //!     -> Name | Phone
+    //!     -> 
+    //! Update
+    //!     -> Not exists then Exception
+    //! Delete
+    //! Get
+
+    internal class UserManagementTest
+    {
+    }
+}
